122fd795da54548319ce35405e064c48b46acb1e
Guilherme Trevizani <tr3v1z4@gmail.com>
2020-02-02 17:11:20 -0300

    Base do emprego de taxista

---
+++ InfiniteRoleplay/Models/Emprego.cs
@@ -0,0 +1,10 @@
+﻿using GTANetworkAPI;
+
+namespace InfiniteRoleplay.Models
+{
+    public class Emprego
+    {
+        public TipoEmprego Tipo { get; set; }
+        public Vector3 Posicao { get; set; }
+    }
+}
